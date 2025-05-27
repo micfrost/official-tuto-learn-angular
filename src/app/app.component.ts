afa221706aae1de6a9da0068e3479fa8f56da6cf
micfro <michalfrost71@gmail.com>
2025-05-27 22:41:53 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import {Component} from '@angular/core';
+import {FormsModule} from "@angular/forms";
+
+@Component({
+    selector: 'app-root',
+    template: `Witaj w Angularze - Jello Universe!
+    <hr>
+    Jestes we wspanialym miescie {{ city }}.
+    <hr>
+    1 +1 = {{ 1 + 1 }}.
+    <hr>
+    <input [(ngModel)]="nazwaUzytkownika">
+    <hr>
+    <button (click)="zapisz()">Zapiszesz w consoli log</button>
+    `,
+    imports: [FormsModule],
+    styles: `:host {
+      color: #a144eb;
+    }`
+})
+
+export class AppComponent {
+    city: string = "Wroclaw";
+
+    nazwaUzytkownika: string = ""
+
+    zapisz() {
+        console.log("Zapisano: " + this.nazwaUzytkownika);
+    }
+
+
+}
